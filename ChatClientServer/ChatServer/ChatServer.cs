ef1721bdb2ca88189f4c6603220b5db2245a19b5
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-09-05 14:11:46 +0200

    Upstart

---
+++ ChatClientServer/ChatServer/ChatServer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class ChatServer
+    {
+        private IPAddress IP = IPAddress.Parse("127.0.0.1");
+        private int port;
+        private volatile bool stop;
+
+        public ChatServer(int port)
+        {
+            this.port = port;
+        }
+
+        internal void Run()
+        {
+            TcpListener listener = new TcpListener(IP, port);
+            listener.Start();
+
+            stop = false;
+
+            while (!stop)
+            {
+                Console.WriteLine("Server is ready for a new client to connect.");
+
+                Socket clientSocket = listener.AcceptSocket();
+
+                Thread ClientHandlerThread = new Thread(new ClientHandler(clientSocket).Start);
+                ClientHandlerThread.Start();
+            }
+
+            Console.WriteLine("Shutting down connection...");
+            listener.Stop();
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ ChatClientServer/ChatServer/ClientHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class ClientHandler
+    {
+        private Socket clientSocket;
+        private string clientName;
+
+        public ClientHandler(Socket clientSocket)
+        {
+            this.clientSocket = clientSocket;
+        }
+
+        internal void Start()
+        {
+            NetworkStream stream = new NetworkStream(clientSocket);
+            StreamWriter writer = new StreamWriter(stream);
+            StreamReader reader = new StreamReader(stream);
+            string clientText;
+
+            writer.WriteLine("The server is ready. Choose a username.");
+            writer.Flush();
+            clientName = reader.ReadLine();
+            writer.WriteLine("Connected as {0}.", clientName);
+            writer.Flush();
+            Console.WriteLine("Client named {0}, connected.", clientName);
+
+            do
+            {
+
+            } while (true);
+        }
+    }
+}
